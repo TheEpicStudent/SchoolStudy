ba431680661b16548e33dc2e60f1bf5bc821408f
TheEpicStudent <168029551+TheEpicStudent@users.noreply.github.com>
2025-04-23 17:53:51 -0500

    Devopment of lofi beats, FINaLLY BEtTEr pAThS, and 404 reporting.

--- server.js
+++ server.js
@@ -38,6 +38,54 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end();
         });
+    } else if (pathname === '/beats' && req.method === 'GET') {
+        const filePath = path.join(__dirname, 'public', 'music.html');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end('Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data, 'utf-8');
+                console.log('got music.html')
+            }
+        });
+    } else if (pathname === '/noisegen' && req.method === 'GET') {
+        const filePath = path.join(__dirname, 'public', 'noisegen.html');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end('Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data, 'utf-8');
+                console.log('got noisegen.html')
+            }
+        });
+    } else if (pathname === '/notes' && req.method === 'GET') {
+        const filePath = path.join(__dirname, 'public', 'notes.html');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end('Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data, 'utf-8');
+                console.log('got notes.html')
+            }
+        });
+    } else if (pathname === '/timer' && req.method === 'GET') {
+        const filePath = path.join(__dirname, 'public', 'timer.html');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/html' });
+                res.end('Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data, 'utf-8');
+                console.log('got timer.html')
+            }
+        });
     } else {
         let filePath = path.join(__dirname, 'public', pathname === '/' ? 'index.html' : pathname);
         const extname = String(path.extname(filePath)).toLowerCase();
@@ -49,6 +97,7 @@ const server = http.createServer((req, res) => {
                     fs.readFile(path.join(__dirname, 'public', '404.html'), (error, content) => {
                         res.writeHead(404, { 'Content-Type': 'text/html' });
                         res.end(content, 'utf-8');
+                        console.error(`got 404 at ${filePath}`)
                     });
                 } else {
                     res.writeHead(500);
@@ -64,4 +113,6 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, () => {
     console.log(`Server is listening on port http://localhost:${port}`);
+    console.info(`Server is running in ${process.env.NODE_ENV || 'development'} mode`);
+    console.log('Btw if you didnt know, you can press Ctrl+C to stop the server');
 });
